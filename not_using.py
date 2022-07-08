7ab0ba06feaeef3775b958254dc01f219fe2fbdb
rajeevsharmacoder <rajeevsharma2129@gmail.com>
2022-07-08 14:45:25 +0530

    galaxy-game

---
+++ not_using.py
@@ -0,0 +1,22 @@
+def on_parent(self, widget, parent):
+    # print("On PARENT W: " + str(self.width) + " H: " + str(self.height))
+    pass
+
+
+def on_size(self, *args):
+    # print("On SIZE W: " + str(self.width) + " H: " + str(self.height))
+    # self.perspective_point_x = self.width / 2
+    # self.perspective_point_y = self.height * 3 / 4
+    # self.update_vertical_lines()
+    # self.update_horizontal_lines()
+    pass
+
+
+def on_perspective_point_x(self, widget, value):
+    # print("Perspective X: " + str(value))
+    pass
+
+
+def on_perspective_point_y(self, widget, value):
+    # print("Perspective Y: " + str(value))
+    pass
